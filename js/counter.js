8cb60fd198233eda3b983517f9c7a8b11dfb2a73
Kodeeshwari <kodeeshwari09@gmail.com>
2018-11-27 19:26:12 -0500

    update text style

--- js/counter.js
+++ js/counter.js
@@ -1,14 +1,12 @@
 $(document).ready(function () {
-    var x=0;
-    var txt=112;    
+    var x=0;  
+    
     $(".minus").click(function () {
-        
         var counterValue = $("#symbol-result").text();
-        var icounterValue=parseInt(counterValue);
+    var icounterValue=parseInt(counterValue);
         if(icounterValue==0){
             $(".result").css({
-            "border-width":2,
-            "display":"table-cell"
+            "border-width":2
         });
             alert("Counter Can't Go For Negative Value");
             return;
@@ -16,19 +14,22 @@ $(document).ready(function () {
         var count=icounterValue-1;
         $("#symbol-result").text(count);
         $(".result").css({
-            "border-width":x-=20,
-            "display":"table-cell"
+            "border-width":x-=10
         });
     });
 
     $(".plus").click(function () {
         var counterValue = $("#symbol-result").text();
         var icounterValue=parseInt(counterValue);
+        if(icounterValue==10){
+            alert("Counter Can't Go Further More");
+            return icounterValue;
+        }
         var count=icounterValue+1;
         $("#symbol-result").text(count);
         $(".result").css({
-            "border-width":x+=20,
-            "display":"table-cell"
+            "border-width":x+=10
+            
         });   
     });
 });
